2b96008f20e8748396e128972d0d3f2e132a99ae
Lisandro Guerra <lisandro.digital@gmail.com>
2023-01-09 21:33:02 +0000

    result panel

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ new Vue({
     el: '#app',
     data: {
         playerLife: 40,
-        monsterLife: 10,
+        monsterLife:10,
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
 
